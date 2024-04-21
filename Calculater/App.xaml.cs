a16c11256b214870ac779b137bc473ac879951d4
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-22 00:45:37 +0300

    Add project files.

---
+++ Calculater/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Calculater
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
